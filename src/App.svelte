4060169f4ae84da0eb7bc11d5026e859d5b0c605
claire <yue.katou@gmail.com>
2023-10-31 14:22:29 -0700

    comments out all helper functions for troubleshooting

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,19 @@
 <script>
   import Icon from "./components/Icon.svelte";
   import Popup from "./components/Popup.svelte";
-  import { debounce, setStorageByComm } from "./scripts/helpers";
+  // import { debounce, setStorageByComm } from "./scripts/helpers";
 
   // ELEMENTS
   const currentUsername = document.querySelectorAll("form span.ljuser")[0];
   const regex = /(?:https:\/\/)?(?:([^.]+)\.)?dreamwidth\.org/;
   const currentComm = window.location.href.match(regex)[1];
   const textarea = document.querySelector("textarea");
+
+  let isClicked = false;
+  const clickIcon = () => {
+    console.log("was clicked");
+    !isClicked;
+  };
 
   // textarea.addEventListener(
   //   "keyup",
@@ -18,4 +24,5 @@
   // );
 </script>
 
-<Icon />
+<Popup {isClicked} />
+<Icon {clickIcon} />
