0ecbb0b051aef26e26da035e2c491d26bf5eb60d
claire <yue.katou@gmail.com>
2023-11-20 17:09:39 -0600

    refactors css for table and hover functionality; adds event listeners to table rows

--- src/components/Popup.svelte
+++ src/components/Popup.svelte
@@ -1,29 +1,35 @@
 <script>
-  import TailwindCss from "./tailwindCSS.svelte";
   export let isClicked;
   export let records;
+  export let selectTag;
+  export let reset;
 
-  console.log(records);
+  import { formatDate } from "./../scripts/helpers";
 
-  const constantClasses = "container mx-auto px-4 rounded";
+  const constantClasses = "wrap";
   const visible = `${constantClasses} visible`;
   const hide = `${constantClasses} hide`;
 </script>
 
-<TailwindCss />
-<div class="wrap">
-  <div id="tag-storage-container" class={isClicked ? visible : hide}>
-    <table class="hover:table-fixed">
+<div class={isClicked ? visible : hide}>
+  <div id="tag-storage-container">
+    <table role="grid">
       <tbody>
         {#if !records}
           <tr>
             <td><p>Loading....</p></td>
           </tr>
         {:else}
-          {#each records.reverse() as record}
-            <tr>
-              <td>{record.tag}</td>
-              <td>{record.timestamp}</td>
+          {#each records.reverse() as record, index}
+            <tr
+              on:mouseover={() => selectTag(record.tag, index)}
+              on:focus={() => selectTag(record.tag, index)}
+              on:mouseleave={reset}
+              on:focusout={reset}
+            >
+              <td class="tag-text">{record.tag}</td>
+              <td class="timestamp">{formatDate(new Date(record.timestamp))}</td
+              >
             </tr>
           {/each}
         {/if}
@@ -34,7 +40,7 @@
 
 <style>
   .visible {
-    float: left;
+    display: block;
   }
   .hide {
     display: none;
@@ -42,12 +48,56 @@
   .wrap {
     position: relative;
   }
+  .tag-text {
+    font-family: monospace, "Courier New", Courier;
+    width: 350px;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+  .timestamp {
+    width: 200px;
+    text-align: right;
+    margin-right: 10px;
+  }
   #tag-storage-container {
-    width: 400px;
-    height: 200px;
+    margin: 0 auto;
+    padding: 0.2em;
+    width: 550px;
+    height: 250px;
     position: absolute;
     z-index: 3;
-    top: -250px;
-    background-color: gray;
+    top: -270px;
+    border-radius: 8px;
+    background-color: white;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    border: 1px solid rgba(0, 0, 0, 0.085);
+  }
+  ::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    background-color: rgba(0, 0, 0, 0.5);
+    -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+  }
+  #tag-storage-container table {
+    width: 100%;
+    table-layout: fixed;
+  }
+  #tag-storage-container table tr {
+    border-bottom: 1px rgba(0, 9, 40, 0.085);
+    border-top: 1px rgba(0, 9, 40.085);
+  }
+  #tag-storage-container table tr:nth-child(even) {
+    background-color: rgba(0, 9, 40, 0.04);
+  }
+  #tag-storage-container table tr:hover,
+  #tag-storage-container table tr:nth-child(even):hover {
+    background-color: rgba(0, 9, 40, 0.07);
   }
 </style>
