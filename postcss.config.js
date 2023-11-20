c1204d6e9a7ef574c89171e4555b332dc9588701
claire <yue.katou@gmail.com>
2023-11-20 10:51:50 -0600

    removes tailwind css and adds pico css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,4 @@
-import tailwind from "tailwindcss";
-import tailwindConfig from "./tailwind.config.js";
 import autoprefixer from "autoprefixer";
 export default {
-  plugins: [tailwind(tailwindConfig), autoprefixer],
+  plugins: [],
 };
