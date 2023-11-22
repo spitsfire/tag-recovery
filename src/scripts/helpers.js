3adcdda4d41299696e4c16f660168276cdbd9d19
claire <yue.katou@gmail.com>
2023-11-22 12:29:34 -0600

    refactors setStorage to shorten the amt of parameters it takes

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,3 +1,4 @@
+import { records } from "./../store/records.store";
 /* 
 WAITS FOR KEYUP EVENT TIMEOUT
 IF KEYUP RESUMES, TIMEOUT IS RESET
@@ -19,7 +20,6 @@ BY CURRENT USER'S COMMUNITY SUBDOMAIN
 export const loadStorage = (username) => {
   const result = JSON.parse(localStorage.getItem(username));
   if (result) {
-    console.log(result);
     return result;
   } else {
     return undefined;
@@ -37,13 +37,12 @@ export const filterByUsername = (storage, username) => {
 SAVES NEW TEXTAREA DATA 
 BY CURRENT USER'S COMMUNITY SUBDOMAIN
 */
-export const setStorage = (comm, username, newTag, userStorage) => {
-  if (userStorage) {
-    const tempStorage = [...userStorage];
+export const setStorage = ({ comm, username }, newTag, records) => {
+  if (records) {
+    const tempStorage = [...records];
     tempStorage.push(newTag);
     localStorage.setItem(username, JSON.stringify(tempStorage));
   } else {
-    console.log("else", newTag);
     localStorage.setItem(username, JSON.stringify([newTag]));
   }
 };
