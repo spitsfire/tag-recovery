4b007f833728b95e5e29c2a6393a59ab124d0f76
claire <yue.katou@gmail.com>
2023-11-29 14:47:44 -0800

    picks correct anchor based on dreamwidth's reply footer

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 // import "./app.css";
 import App from "./App.svelte";
 
+function findElementId() {
+  return (
+    document.getElementById("previewplaceholder") ||
+    document.getElementsByName("submitpreview")[0]
+  );
+}
+
 const app = new App({
   target: document.querySelector(".qr-footer"),
-  anchor: document.getElementById("previewplaceholder"),
+  anchor: findElementId(),
   hydrate: false,
 });
 
